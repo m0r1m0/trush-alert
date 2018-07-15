50f741a1c6bb5f5a80a965ebb6e1bbd1492cd112
pywd <yuki.wd7@gmail.com>
2018-07-15 17:21:59 +0900

    Add drawer #3

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,7 +1,9 @@
 import * as React from 'react';
+
 import { AppHeader } from './organisms/AppHeader';
 import { LayoutState } from '../store/reducers/layoutReducer';
 import { LayoutActions } from '../container/App';
+import { AppDrawer } from './organisms/AppDrawer';
 
 interface OwnProps {}
 
@@ -10,7 +12,8 @@ type AppProps = OwnProps & LayoutState & LayoutActions;
 export const App: React.SFC<AppProps> = (props: AppProps) => {
   return (
     <div className="App">
-      <AppHeader {...props} />
+      <AppHeader toggleMenu={props.toggleMenu} />
+      <AppDrawer {...props} />
       <p>Page Contents</p>
     </div>
   );
