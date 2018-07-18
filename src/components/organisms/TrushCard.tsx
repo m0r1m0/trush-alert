7b57dba5d11bb657590b22dcc32aa46a2c84850a
pywd <yuki.wd7@gmail.com>
2018-07-19 00:39:51 +0900

    Add expand state and action to App #12

--- src/components/organisms/TrushCard.tsx
+++ src/components/organisms/TrushCard.tsx
@@ -6,11 +6,32 @@ import {
   Typography,
   CardActions,
   IconButton,
+  Collapse,
 } from '@material-ui/core';
 import styled from 'styled-components';
 import { TrushState } from '../../store/reducers/trushReducer';
 import { getTrushType } from '../../util/getTrushType';
 import ExpandMore from '@material-ui/icons/ExpandMore';
+import { Action } from 'typescript-fsa';
+
+interface TrushCardAction {
+  toggleExpand: (expand: boolean) => Action<boolean>;
+}
+
+interface TrushCardState {
+  detailExpanded: boolean;
+}
+
+type TrushCardProps = TrushCardAction & TrushState & TrushCardState;
+
+interface TrushType {
+  name: string;
+  image: string;
+}
+
+interface ExpandButtonProps {
+  expanded: boolean;
+}
 
 const TrushImage = styled(CardMedia)`
   padding-top: 60%;
@@ -20,18 +41,18 @@ const TrushImage = styled(CardMedia)`
 `;
 
 const ExpandButton = styled(IconButton)`
+  transform: rotate(
+    ${(props: ExpandButtonProps) => (props.expanded ? 180 : 0)}deg
+  );
   && {
+    transition: transform 150ms cubic-bezier(0.4, 0, 1, 1) 0ms;
     margin-left: auto;
   }
 `;
 
-interface TrushType {
-  name: string;
-  image: string;
-}
-
-export const TrushCard: React.SFC<TrushState> = props => {
-  const trushData = props.trushInfo.filter(trush => trush.id === 95)[0];
+export const TrushCard: React.SFC<TrushCardProps> = props => {
+  const { trushInfo, toggleExpand, detailExpanded } = props;
+  const trushData = trushInfo.filter(trush => trush.id === 95)[0];
   const trushType: TrushType = getTrushType(trushData);
   return (
     <Card>
@@ -42,10 +63,18 @@ export const TrushCard: React.SFC<TrushState> = props => {
         </Typography>
       </CardContent>
       <CardActions disableActionSpacing={true}>
-        <ExpandButton>
+        <ExpandButton
+          expanded={detailExpanded}
+          onClick={() => toggleExpand(!detailExpanded)}
+        >
           <ExpandMore />
         </ExpandButton>
       </CardActions>
+      <Collapse in={detailExpanded} timeout="auto">
+        <CardContent>
+          <p>Collapse Content</p>
+        </CardContent>
+      </Collapse>
     </Card>
   );
 };
