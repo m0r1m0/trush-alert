7c00e429b4cb712698d6064ad3e6fe7abd0b41bf
pywd <yuki.wd7@gmail.com>
2018-07-18 23:47:24 +0900

    Add expand button #12

--- src/components/organisms/TrushCard.tsx
+++ src/components/organisms/TrushCard.tsx
@@ -1,13 +1,27 @@
 import * as React from 'react';
-import { Card, CardMedia, CardContent, Typography } from '@material-ui/core';
+import {
+  Card,
+  CardMedia,
+  CardContent,
+  Typography,
+  CardActions,
+  IconButton,
+} from '@material-ui/core';
 import styled from 'styled-components';
 import { TrushState } from '../../store/reducers/trushReducer';
 import { getTrushType } from '../../util/getTrushType';
+import ExpandMore from '@material-ui/icons/ExpandMore';
 
 const TrushImage = styled(CardMedia)`
   padding-top: 60%;
   && {
     background-size: 50%;
+  }
+`;
+
+const ExpandButton = styled(IconButton)`
+  && {
+    margin-left: auto;
   }
 `;
 
@@ -27,6 +41,11 @@ export const TrushCard: React.SFC<TrushState> = props => {
           {trushType.name}
         </Typography>
       </CardContent>
+      <CardActions disableActionSpacing={true}>
+        <ExpandButton>
+          <ExpandMore />
+        </ExpandButton>
+      </CardActions>
     </Card>
   );
 };
